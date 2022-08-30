c80d281cc2c0ff0f696ddfaa4531009017370731
pravinkumar09 <pravin.k@codingmart.com>
2022-08-30 09:00:49 +0530

    Minor Changes with form

--- Day 1/muldiv/index.js
+++ Day 1/muldiv/index.js
@@ -1,9 +1,17 @@
-let a = prompt("Enter the first number")
-let b = prompt("Enter the second number")
-let mul = a * b
-let div = a / b
-let rem = a % b
-if (a < b) {
-    rem = b
+let a = 0
+let b = 0
+
+function dotest() {
+    console.log("submit")
+    console.log("test")
+    a = document.getElementById("fnum").value
+    b = document.getElementById("snum").value
+
+    let mul = a * b
+    let div = a / b
+    let rem = a % b
+    if (a < b) {
+        rem = b
+    }
+    document.body.innerHTML += "<br>" + "Product:" + mul + "<br>" + "Quotient:" + parseInt(div) + "<br>" + "Remainder:" + rem
 }
-document.body.innerHTML += "Product:" + mul + "<br>" + "Quotient:" + parseInt(div) + "<br>" + "Remainder:" + rem
